48b80122f043925e62a9b426f84c5c9f97c43d0a
sainikith07 <155831198+sainikith07@users.noreply.github.com>
2025-05-07 11:04:12 +0530

    Navigation between pages

---
+++ Navigation.dart
@@ -0,0 +1,89 @@
+//main.dart
+import 'package:flutter/material.dart';
+import 'page1.dart';
+import 'page2.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Named Routes Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/page1': (context) => Page1(),
+        '/page2': (context) => Page2(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Main Page")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/page1');
+              },
+              child: Text("Go to Page 1"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/page2');
+              },
+              child: Text("Go to Page 2"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+//page1.dart
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Page 1")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text("Back"),
+        ),
+      ),
+    );
+  }
+}
+
+//page2.dart
+import 'package:flutter/material.dart';
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Page 2")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text("Back"),
+        ),
+      ),
+    );
+  }
+}
+
